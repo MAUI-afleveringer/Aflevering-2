17f47b4929da36757ddf52a9c6912cc8667c1172
Filip Allan Alberg <filipalberg99@gmail.com>
2025-05-06 15:59:27 +0200

    Co-authored-by: Nicolaj Meldgaard Pedersen <NicolajMeldgaard@users.noreply.github.com>

--- frontend/ModelManagement/src/Managers.jsx
+++ frontend/ModelManagement/src/Managers.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+
 export default function Managers() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:8080/api/managers',
+                {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        Authorization: 'bearer ' + token,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            const result = await response.json();
+            setData(result);
+            console.log('data is set');
+        }
+        fetchData();
+
+    }, []); // Run the effect (fetch in this case) only once when the component renders
+
     return (
-        <h1>Man</h1>
+        <main>
+            <h1>Managers</h1>
+            {
+                data ?
+                    data.map(manager => (
+                        <section className="manager">
+                            <p className="managerId">{manager.managerId}</p>
+                            <p className="managerName">{manager.firstName} {manager.lastName}</p>
+                            <p className="managerEmail">{manager.email}</p>
+                        </section>
+                    ))
+                    : <p>Loading...</p>
+            }
+        </main>
     )
 }
