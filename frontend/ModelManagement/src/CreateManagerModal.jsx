dcef8defc04e3e2543a7870cc3fca30132562c45
Filip Allan Alberg <filipalberg99@gmail.com>
2025-05-07 13:58:23 +0200

    hotfixes

--- frontend/ModelManagement/src/CreateManagerModal.jsx
+++ frontend/ModelManagement/src/CreateManagerModal.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react'
+
 export default function ManagerModal({ onClose }) {
+
+    const { firstName, setFirstName } = useState("");
+    const { lastName, setLastName } = useState("");
+    const { email, setEmail } = useState("");
+    const { password, setPassword } = useState("");
+    const { error, setError } = useState("");
+
+
+
     return (
         <>
             <div className="dark"></div>
@@ -11,7 +22,7 @@ export default function ManagerModal({ onClose }) {
                     <input type="password" name="" id="" className="formInput" placeholder="Password" />
                     <div className="createBtns">
                         <input type="button" value="Create Manager" />
-                        <input type="button" value="Cancel" onClick={onClose} />
+                        <input type="button" value="Close" onClick={onClose} />
                     </div>
                 </form>
             </section>
